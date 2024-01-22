3970676284bfb0b1a1798499933e3a4e146f1bfb
Gilberto <gilberto.lizbar@gmail.com>
2024-01-21 22:19:46 -0600

    surefire-plugin in POM to trigger test using maven commands
    - Create Profiles in POM to run test suites labeled by ids and run in terminal using maven commands like mvn test -PRegression  through maven-surefire-plugin
    - Add group annotations to tests
    - Add test class to test smoke and regression labels

--- src/test/java/com/mobileframework/InitialTests_iOS.java
+++ src/test/java/com/mobileframework/InitialTests_iOS.java
@@ -2,18 +2,18 @@ package com.mobileframework;
 
 import org.testng.annotations.Test;
 
-import com.mobileframework.TestUtils.BaseClass1;
+import com.mobileframework.TestUtils.BaseClass;
 import com.mobileframework.pageObjects.ios.InitialPage_iOS;
 
-public class InitialTests_iOS extends BaseClass1 {
+public class InitialTests_iOS extends BaseClass {
 	
-	@Test
+	@Test(groups = {"Smoke"})
 	public void firstTest_iOS(){
 		InitialPage_iOS firstPage = new InitialPage_iOS(driver);
 		firstPage.clickAlertViewButton();
 	}
 	
-	@Test
+	@Test(groups = {"Smoke"})
 	public void secondTest_iOS(){
 		InitialPage_iOS firstPage = new InitialPage_iOS(driver);
 		firstPage.clickAlertViewButton();
